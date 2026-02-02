543ab150e4194498e6021c5bd357e99325b8e153
HafzaO <hafzaosmankhan1234@gmail.com>
2026-02-02 14:24:52 -0500

    Update BlueTeleOpLL.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BlueTeleOpLL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BlueTeleOpLL.java
@@ -123,8 +123,8 @@ public class BlueTeleOpLL extends OpMode {
     // AprilTag tracking variables
     private Limelight3A limelight;
     private AnalogInput encoder;
-    private double kP = 0.095 ;
-    private double max = 0.01;
+    private double kP = 0.09 ;
+    private double max = 0.04;
     private boolean aprilTagTracking = false;
     private boolean debounceRightStick;
 
