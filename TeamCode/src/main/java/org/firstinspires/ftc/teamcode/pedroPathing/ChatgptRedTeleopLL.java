28d9734cb1f2d30d319046eed1cecf55b2c4b0a5
moosecoding <moosecoding@moosecoding.my.domain>
2026-02-09 17:14:26 -0500

    Photon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ChatgptRedTeleopLL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ChatgptRedTeleopLL.java
@@ -5,6 +5,7 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -13,6 +14,8 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import photoncore.PhotonCore;
 
 @Configurable
 @TeleOp
@@ -121,9 +124,15 @@ public class ChatgptRedTeleopLL extends OpMode {
         raxon.setPosition(.48);
         laxon.setPosition(.48);
 
+        PhotonCore.CONTROL_HUB.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        PhotonCore.EXPANSION_HUB.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        PhotonCore.experimental.setMaximumParallelCommands(6);
+        PhotonCore.experimental.setSinglethreadedOptimized(false);
+        PhotonCore.enable();
 
 
-
+        PhotonCore.CONTROL_HUB.clearBulkCache();
+        PhotonCore.EXPANSION_HUB.clearBulkCache();
     }
 
     @Override
