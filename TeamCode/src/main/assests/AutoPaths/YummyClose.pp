a7ea8b1934d72d66ccbdb50567aedccdd3cd1d65
1G-N0R <Aadhithya.Padmanaban@gmail.com>
2026-03-05 16:14:24 -0500

    Create YummyClose.pp

---
+++ TeamCode/src/main/assests/AutoPaths/YummyClose.pp
@@ -0,0 +1,238 @@
+{
+  "startPoint": {
+    "x": 24,
+    "y": 120,
+    "heading": "constant",
+    "startDeg": 180,
+    "endDeg": 180,
+    "locked": false,
+    "degrees": 90
+  },
+  "lines": [
+    {
+      "id": "mmdxpfjk-9wkwlc",
+      "name": "",
+      "endPoint": {
+        "x": 50,
+        "y": 84,
+        "heading": "linear",
+        "reverse": false,
+        "degrees": 0,
+        "startDeg": 90,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#AA89BA",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mmdxr77e-arhvjh",
+      "name": "",
+      "endPoint": {
+        "x": 24,
+        "y": 84,
+        "heading": "constant",
+        "reverse": false,
+        "degrees": 180
+      },
+      "controlPoints": [],
+      "color": "#7CD7BC",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mmdxs3d1-gedxfw",
+      "name": "",
+      "endPoint": {
+        "x": 60,
+        "y": 75,
+        "heading": "linear",
+        "reverse": false,
+        "degrees": 0,
+        "startDeg": 180,
+        "endDeg": 135
+      },
+      "controlPoints": [],
+      "color": "#AA866C",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mmdxtleb-tmf9k2",
+      "name": "",
+      "endPoint": {
+        "x": 24,
+        "y": 60,
+        "heading": "linear",
+        "reverse": false,
+        "degrees": 0,
+        "startDeg": 135,
+        "endDeg": 180
+      },
+      "controlPoints": [
+        {
+          "x": 60,
+          "y": 60
+        }
+      ],
+      "color": "#A9BC78",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mmdxzu6c-ijwdjm",
+      "name": "",
+      "endPoint": {
+        "x": 56,
+        "y": 82,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 135
+      },
+      "controlPoints": [],
+      "color": "#BB969D",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mmdy1ml3-xqbzir",
+      "name": "",
+      "endPoint": {
+        "x": 10,
+        "y": 60,
+        "heading": "constant",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 135,
+        "degrees": 135
+      },
+      "controlPoints": [],
+      "color": "#DB77B5",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mmdy49yl-9q9xv0",
+      "name": "",
+      "endPoint": {
+        "x": 56,
+        "y": 82,
+        "heading": "constant",
+        "reverse": false,
+        "degrees": 135
+      },
+      "controlPoints": [],
+      "color": "#D8B99C",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    }
+  ],
+  "shapes": [
+    {
+      "id": "triangle-1",
+      "name": "Red Goal",
+      "vertices": [
+        {
+          "x": 144,
+          "y": 69.5
+        },
+        {
+          "x": 144,
+          "y": 144
+        },
+        {
+          "x": 119,
+          "y": 144
+        },
+        {
+          "x": 137.5,
+          "y": 119
+        },
+        {
+          "x": 137.5,
+          "y": 69.5
+        }
+      ],
+      "color": "#dc2626",
+      "fillColor": "#fca5a5",
+      "type": "obstacle"
+    },
+    {
+      "id": "triangle-2",
+      "name": "Blue Goal",
+      "vertices": [
+        {
+          "x": 6.5,
+          "y": 119
+        },
+        {
+          "x": 25,
+          "y": 144
+        },
+        {
+          "x": 0,
+          "y": 144
+        },
+        {
+          "x": 0,
+          "y": 69.5
+        },
+        {
+          "x": 6.5,
+          "y": 69.5
+        }
+      ],
+      "color": "#0b08d9",
+      "fillColor": "#fca5a5",
+      "type": "obstacle"
+    }
+  ],
+  "sequence": [
+    {
+      "kind": "path",
+      "lineId": "mmdxpfjk-9wkwlc"
+    },
+    {
+      "kind": "path",
+      "lineId": "mmdxr77e-arhvjh"
+    },
+    {
+      "kind": "path",
+      "lineId": "mmdxs3d1-gedxfw"
+    },
+    {
+      "kind": "path",
+      "lineId": "mmdxtleb-tmf9k2"
+    },
+    {
+      "kind": "path",
+      "lineId": "mmdxzu6c-ijwdjm"
+    },
+    {
+      "kind": "path",
+      "lineId": "mmdy1ml3-xqbzir"
+    },
+    {
+      "kind": "path",
+      "lineId": "mmdy49yl-9q9xv0"
+    }
+  ],
+  "version": "1.2.1",
+  "timestamp": "2026-03-05T21:14:14.127Z"
+}
