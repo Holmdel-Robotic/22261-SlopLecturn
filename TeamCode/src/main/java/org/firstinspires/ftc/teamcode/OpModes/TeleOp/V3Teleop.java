7e550d57adf27715db94945ca73ecf6efed78f38
Grant <grantlin08@gmail.com>
2026-03-08 12:41:26 -0400

    Update V3Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/V3Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/V3Teleop.java
@@ -94,21 +94,57 @@ public class V3Teleop extends OpMode {
 
     private void driveRobot() {
 
-        double y = -gamepad1.left_stick_y;
-        double x = gamepad1.left_stick_x * 1.1;
-        double rx = gamepad1.right_stick_x * .8;
+        if(DriveMode) {
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x * .8;
 
 
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
-        frontLeftMotor.setPower((y + x + rx) / denominator);
+            frontLeftMotor.setPower((y + x + rx) / denominator);
 
-        backLeftMotor.setPower((y - x + rx) / denominator);
+            backLeftMotor.setPower((y - x + rx) / denominator);
 
-        frontRightMotor.setPower((y - x - rx) / denominator);
+            frontRightMotor.setPower((y - x - rx) / denominator);
 
-        backRightMotor.setPower((y + x - rx) / denominator);
+            backRightMotor.setPower((y + x - rx) / denominator);
+        }else {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
 
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers.
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+        }
+        }
     }
 
     private void WholeIntakeOperation(boolean gamepad){
