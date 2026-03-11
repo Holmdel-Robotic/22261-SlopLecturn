9dc81d668bba384364937e34fd94dc84f281860f
1G-N0R <Aadhithya.Padmanaban@gmail.com>
2026-03-11 12:13:08 -0400

    Adding actually shooting stuff to YummyDemo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/YummyDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/YummyDemo.java
@@ -10,6 +10,8 @@
 
 package org.firstinspires.ftc.teamcode.OpModes.Auto;
 
+import android.sax.EndElementListener;
+
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
@@ -22,21 +24,43 @@ import com.pedropathing.geometry.PedroCoordinates;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.HeadingInterpolator;
 import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.State;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+
 
 @Autonomous(name = "Something Something Close Side Pathing", group = "Autonomous")
 @Configurable // Panels
 public class YummyDemo extends OpMode {
 
+    private Servo gate, indicatorLight1, indicatorLight2;
+    private DcMotorEx frontRightMotor, frontLeftMotor, backRightMotor, backLeftMotor;
+    private DcMotorEx flywheelLeft, flywheelRight, intakeOuter, intakeInner;
+
+    private DistanceSensor intakeSensor1, intakeSensor2;
+    private boolean getLine;
+    private Servo hood, raxon, laxon;
     private TelemetryManager panelsTelemetry; // Panels Telemetry instance
     public Follower follower; // Pedro Pathing follower instance
-    private int pathState; // Current autonomous path state (state machine)
+    private State pathState = State.SCORE; // Current autonomous path state (state machine)
     private ElapsedTime pathTimer; // Timer for path state machine
+    private Timer actionTimer;
     private Paths paths; // Paths defined in the Paths class
-
+    private enum State{
+        SCORE,
+        PICKUP1,
+        HUMANZONE,
+        END;
+    }
     @Override
     public void init() {
         panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
@@ -44,20 +68,49 @@ public class YummyDemo extends OpMode {
         panelsTelemetry.debug("Status", "Initialized");
         panelsTelemetry.update(telemetry);
 
+        flywheelLeft = hardwareMap.get(DcMotorEx.class, "flyL");
+        flywheelRight = hardwareMap.get(DcMotorEx.class, "flyR");
+        flywheelRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheelLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheelRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        flywheelLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        flywheelLeft.setDirection(DcMotor.Direction.FORWARD);
+        flywheelRight.setDirection(DcMotor.Direction.REVERSE);
+
+        intakeOuter = hardwareMap.get(DcMotorEx.class, "intOuter");
+        intakeInner = hardwareMap.get(DcMotorEx.class, "intInner");
+        intakeOuter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intakeInner.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intakeOuter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeInner.setDirection(DcMotor.Direction.REVERSE);
+        intakeOuter.setDirection(DcMotor.Direction.REVERSE);
+        intakeInner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        intakeSensor1 = hardwareMap.get(DistanceSensor.class, "intakeSensor1");
+        intakeSensor2 = hardwareMap.get(DistanceSensor.class, "intakeSensor2");
+
+        hood = hardwareMap.get(Servo.class, "hood");
+        raxon = hardwareMap.get(Servo.class, "raxon");
+        laxon = hardwareMap.get(Servo.class, "laxon");
+
         follower = Constants.createFollower(hardwareMap);
         // Determine starting heading: prefer geometric heading when a path exists, otherwise fall back to explicit startPoint values
         follower.setStartingPose(
-                new Pose(48.000, 10.000, Math.toRadians(-176.498))
+                new Pose(48.000, 10.000, Math.toRadians(-180))
         );
 
         pathTimer = new ElapsedTime();
+        actionTimer = new Timer();
         paths = new Paths(follower); // Build paths
+        pathState = State.SCORE;
+        getLine = true;
+
     }
 
     @Override
     public void loop() {
         follower.update(); // Update Pedro Pathing
-        pathState = autonomousPathUpdate(); // Update autonomous state machine
+        autonomousPathUpdate(); // Update autonomous state machine
 
         // Log values to Panels and Driver Station
         panelsTelemetry.debug("Path State", pathState);
@@ -73,6 +126,7 @@ public class YummyDemo extends OpMode {
         public PathChain line2;
         public PathChain line3;
         public PathChain line4;
+        public PathChain line5;
 
         public Paths(Follower follower) {
             line1 = follower
@@ -110,57 +164,97 @@ public class YummyDemo extends OpMode {
                     )
                     .setConstantHeadingInterpolation(Math.toRadians(180))
                     .build();
+
+            line5 = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierLine(new Pose(10,10), new Pose(24,36))
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(225))
+                    .build();
         }
     }
 
-    public int autonomousPathUpdate() {
+    public void autonomousPathUpdate() {
         switch (pathState) {
-            case 0:
-                follower.followPath(paths.line1, true);
-                setPathState(1);
-                break;
-            case 1:
+            case SCORE:
+                raxon.setPosition(.5);
+                laxon.setPosition(.5);
+                flywheelLeft.setVelocity(2000);
+                flywheelRight.setVelocity(2000);
+                hood.setPosition(.5);
+
+                if (follower.isBusy()){
+                    actionTimer.resetTimer();
+                }else{
+                    intakeOuter.setPower(-.8);
+                    intakeInner.setPower(.3);
+                    gate.setPosition(.88);
+                }
+                if (actionTimer.getElapsedTimeSeconds() > 3) {
+                    gate.setPosition(.5);
+                    if(getLine) {
+                        follower.followPath(paths.line1, true);
+                        setPathState(State.PICKUP1);
+                        getLine = false;
+                    }
+                    else
+                    {
+                        follower.followPath(paths.line3, true);
+                        setPathState(State.HUMANZONE);
+                    }
+
+                }
+            case PICKUP1:
                 if (!follower.isBusy()) {
-                    setPathState(2);
+                    follower.followPath(paths.line2, true);
+                    setPathState(State.SCORE);
                 }
                 break;
-            case 2:
-                follower.followPath(paths.line2, true);
-                setPathState(3);
-                break;
-            case 3:
+            case HUMANZONE:
                 if (!follower.isBusy()) {
-                    setPathState(4);
+                    if(pathTimer.seconds() < 2)
+                    {
+                        follower.followPath(paths.line5);
+                        setPathState(State.END);
+                    }
+                    else {
+                        follower.followPath(paths.line4, true);
+                        setPathState(State.SCORE);
+                    }
+
                 }
                 break;
-            case 4:
-                follower.followPath(paths.line3, true);
-                setPathState(5);
-                break;
-            case 5:
-                if (!follower.isBusy()) {
-                    setPathState(6);
-                }
-                break;
-            case 6:
-                follower.followPath(paths.line4, true);
-                setPathState(7);
-                break;
-            case 7:
-                if (!follower.isBusy()) {
-                    setPathState(8);
-                }
-                break;
-            case 8:
+            case END:
                 requestOpModeStop();
-                pathState = -1;
                 break;
+//            case 4:
+//                follower.followPath(paths.line3, true);
+//                setPathState(5);
+//                break;
+//            case 5:
+//                if (!follower.isBusy()) {
+//                    setPathState(6);
+//                }
+//                break;
+//            case 6:
+//                follower.followPath(paths.line4, true);
+//                setPathState(7);
+//                break;
+//            case 7:
+//                if (!follower.isBusy()) {
+//                    setPathState(8);
+//                }
+//                break;
+//            case 8:
+//                requestOpModeStop();
+//                pathState = -1;
+//                break;
         }
-        return pathState;
+
     }
 
-    public void setPathState(int pState) {
+    public void setPathState(State pState) {
         pathState = pState;
-        pathTimer.reset();
     }
 }
