500b365e688247a009eb8b9da5594b764533b128
1G-N0R <Aadhithya.Padmanaban@gmail.com>
2026-03-15 03:51:13 -0400

    Update BlueBigTriangleYummy.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/BlueBigTriangleYummy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/BlueBigTriangleYummy.java
@@ -1,5 +1,6 @@
 
 package org.firstinspires.ftc.teamcode.OpModes.Auto;
+import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.bylazar.configurables.annotations.Configurable;
@@ -11,24 +12,83 @@ import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "Blue Big Triangle Yummy", group = "Autonomous")
 @Configurable // Panels
 public class BlueBigTriangleYummy extends OpMode {
     private TelemetryManager panelsTelemetry; // Panels Telemetry instance
     public Follower follower; // Pedro Pathing follower instance
-    private int pathState; // Current autonomous path state (state machine)
+    private State pathState; // Current autonomous path state (state machine)
     private Paths paths; // Paths defined in the Paths class
+
+    private DcMotorEx frontRightMotor, frontLeftMotor, backRightMotor, backLeftMotor, intakeOuter, intakeInner, flywheelLeft, flywheelRight;
+
+    private Servo hood, raxon, laxon, innerGate, outerGate;
+
+    private Timer actionTimer;
+    private boolean scored = false;
+    private int row = 0;
+
+    private enum State {
+        START,
+        SCORE,
+        COLLECT,
+        END;
+    }
+
+
 
     @Override
     public void init() {
         panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+        panelsTelemetry.debug("Status", "Initialized");
+        panelsTelemetry.update(telemetry);
+
+        frontLeftMotor = hardwareMap.get(DcMotorEx.class, "fl");
+        frontRightMotor = hardwareMap.get(DcMotorEx.class, "fr");
+        backLeftMotor = hardwareMap.get(DcMotorEx.class, "bl");
+        backRightMotor = hardwareMap.get(DcMotorEx.class, "br");
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        intakeOuter = hardwareMap.get(DcMotorEx.class, "intOuter");
+        intakeInner = hardwareMap.get(DcMotorEx.class, "intInner");
+        intakeOuter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intakeInner.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intakeOuter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeInner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeInner.setDirection(DcMotor.Direction.REVERSE);
+        intakeOuter.setDirection(DcMotor.Direction.FORWARD);
+
+        flywheelLeft = hardwareMap.get(DcMotorEx.class, "flyL");
+        flywheelRight = hardwareMap.get(DcMotorEx.class, "flyR");
+        flywheelLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheelRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheelLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        flywheelRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        flywheelLeft.setDirection(DcMotor.Direction.FORWARD);
+        flywheelRight.setDirection(DcMotor.Direction.REVERSE);
+
+        hood = hardwareMap.get(Servo.class, "hood");
+        raxon = hardwareMap.get(Servo.class, "raxon");
+        laxon = hardwareMap.get(Servo.class, "laxon");
+        innerGate = hardwareMap.get(Servo.class, "innerGate");
+        outerGate = hardwareMap.get(Servo.class, "outerGate");
+        hood.setDirection(Servo.Direction.REVERSE);
 
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(new Pose(72, 8, Math.toRadians(90)));
-
+        actionTimer = new Timer();
         paths = new Paths(follower); // Build paths
-
+        pathState = State.START;
         panelsTelemetry.debug("Status", "Initialized");
         panelsTelemetry.update(telemetry);
     }
@@ -54,9 +114,13 @@ public class BlueBigTriangleYummy extends OpMode {
         public PathChain Path5;
         public PathChain Path6;
         public PathChain Path7;
+        public PathChain Path8;
+        public PathChain Path9;
         public PathChain Path10;
 
-        public PathChain Path8;
+        public PathChain Path2;
+
+
 
 
         public Paths(Follower follower) {
@@ -70,7 +134,7 @@ public class BlueBigTriangleYummy extends OpMode {
 
                     .build();
 
-            Path3 = follower.pathBuilder().addPath(
+            Path2 = follower.pathBuilder().addPath(
                             new BezierLine(
                                     new Pose(48.600, 84.700),
 
@@ -80,7 +144,7 @@ public class BlueBigTriangleYummy extends OpMode {
 
                     .build();
 
-            Path4 = follower.pathBuilder().addPath(
+            Path3 = follower.pathBuilder().addPath(
                             new BezierLine(
                                     new Pose(19.000, 84.700),
 
@@ -90,7 +154,7 @@ public class BlueBigTriangleYummy extends OpMode {
 
                     .build();
 
-            Path5 = follower.pathBuilder().addPath(
+            Path4 = follower.pathBuilder().addPath(
                             new BezierCurve(
                                     new Pose(48.600, 84.700),
                                     new Pose(43.092, 57.427),
@@ -100,7 +164,7 @@ public class BlueBigTriangleYummy extends OpMode {
 
                     .build();
 
-            Path6 = follower.pathBuilder().addPath(
+            Path5 = follower.pathBuilder().addPath(
                             new BezierLine(
                                     new Pose(23.855, 60.169),
 
@@ -110,8 +174,28 @@ public class BlueBigTriangleYummy extends OpMode {
 
                     .build();
 
+            Path6 = follower.pathBuilder().addPath(
+                            new BezierLine(
+                                    new Pose(48.600, 84.700),
+
+                                    new Pose(12.300, 60.700)
+                            )
+                    ).setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(150))
+
+                    .build();
+
             Path7 = follower.pathBuilder().addPath(
                             new BezierLine(
+                                    new Pose(12.300, 60.700),
+
+                                    new Pose(48.600, 84.700)
+                            )
+                    ).setLinearHeadingInterpolation(Math.toRadians(150), Math.toRadians(180))
+
+                    .build();
+
+            Path8 = follower.pathBuilder().addPath(
+                            new BezierLine(
                                     new Pose(48.600, 84.700),
 
                                     new Pose(12.300, 60.700)
@@ -120,7 +204,7 @@ public class BlueBigTriangleYummy extends OpMode {
 
                     .build();
 
-            Path10 = follower.pathBuilder().addPath(
+            Path9 = follower.pathBuilder().addPath(
                             new BezierLine(
                                     new Pose(12.300, 60.700),
 
@@ -131,26 +215,6 @@ public class BlueBigTriangleYummy extends OpMode {
                     .build();
 
             Path10 = follower.pathBuilder().addPath(
-                            new BezierLine(
-                                    new Pose(48.600, 84.700),
-
-                                    new Pose(12.300, 60.700)
-                            )
-                    ).setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(150))
-
-                    .build();
-
-            Path8 = follower.pathBuilder().addPath(
-                            new BezierLine(
-                                    new Pose(12.300, 60.700),
-
-                                    new Pose(48.600, 84.700)
-                            )
-                    ).setLinearHeadingInterpolation(Math.toRadians(150), Math.toRadians(180))
-
-                    .build();
-
-            Path8 = follower.pathBuilder().addPath(
                             new BezierLine(
                                     new Pose(48.600, 84.700),
 
@@ -164,9 +228,88 @@ public class BlueBigTriangleYummy extends OpMode {
 
 
     public void autonomousPathUpdate() {
-        // Add your state machine Here
-        // Access paths with paths.pathName
-        // Refer to the Pedro Pathing Docs (Auto Example) for an example state machine
+        switch (pathState) {
+            case START:
+                actionTimer.resetTimer();
+                raxon.setPosition(.5);
+                laxon.setPosition(.5);
+                flywheelLeft.setVelocity(2000);
+                flywheelRight.setVelocity(2000);
+                intakeOuter.setPower(.9);
+                intakeInner.setPower(.8);
+                follower.followPath(paths.Path1, true);
+                setPathState(State.SCORE);
+                break;
+
+            case SCORE:
+                if (follower.isBusy()) {
+                    actionTimer.resetTimer();
+                }
+                else {
+                    innerGate.setPosition(.575);
+                    outerGate.setPosition(.6);
+                    if (actionTimer.getElapsedTimeSeconds() > 2) {
+                        intakeInner.setPower(0);
+                        innerGate.setPosition(.2);
+                        outerGate.setPosition(0);
+                        if (row == 0) {
+                            follower.followPath(paths.Path2, true);
+                            row++;
+                            setPathState(State.COLLECT);
+                        }
+                        else if (row == 1) {
+                            follower.followPath(paths.Path4, true);
+                            row++;
+                            setPathState(State.COLLECT);
+                        }
+                        else if (row == 2) {
+                            follower.followPath(paths.Path6, true);
+                            row++;
+                            setPathState(State.COLLECT);
+                        }
+                        else if (row == 3){
+                            follower.followPath(paths.Path8, true);
+                            row++;
+                            setPathState(State.COLLECT);
+                        }
+                        else
+                        {
+                            follower.followPath(paths.Path10, true);
+                            setPathState(State.END);
+                        }
+                    }
+                }
+                break;
+
+            case COLLECT:
+                if (!follower.isBusy()) {
+                    if (row == 1) {
+                        follower.followPath(paths.Path3, true);
+                    }
+                    else if (row == 2) {
+                        follower.followPath(paths.Path5, true);
+                    }
+                    else if(row == 3) {
+                        follower.followPath(paths.Path7, true);
+                    }
+                    else {
+                        follower.followPath(paths.Path9, true);
+                    }
+                    setPathState(State.SCORE);
+                }
+                break;
+
+            case END:
+                if (!follower.isBusy()) {
+                    requestOpModeStop();
+                }
+                break;
+        }
+    }
+
+    public void setPathState(State pState) {
+        pathState = pState;
+//        pathTimer.reset();
     }
 }
     
