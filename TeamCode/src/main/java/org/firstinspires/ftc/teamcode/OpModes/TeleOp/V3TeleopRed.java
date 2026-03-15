7b024f417933a1e0fb1d6e520f9da64a63fc8f3f
Grant <grantlin08@gmail.com>
2026-03-14 22:33:00 -0400

    Update V3TeleopRed.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/V3TeleopRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/V3TeleopRed.java
@@ -139,7 +139,6 @@ public class V3TeleopRed extends OpMode {
             // Rotate the movement direction counter to the bot's rotation
             double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
             double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
             rotX = rotX * 1.1;  // Counteract imperfect strafing
 
             // Denominator is the largest motor power (absolute value) or 1
